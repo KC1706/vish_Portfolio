0576f3b320546aecb10d8e2f0203528c765db71b
Kc23mj <kunalchandoa925@gmail.com>
2025-05-10 23:20:12 +0530

    done

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -9,6 +9,6 @@ export default defineConfig({
   schema: "./shared/schema.ts",
   dialect: "postgresql",
   dbCredentials: {
-    url: process.env.DATABASE_URL,
+    url: "https://vish-portfolio.onrender.com",
   },
 });
